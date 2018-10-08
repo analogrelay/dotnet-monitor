3d062861efb9e41eef3a8a328fd8267265feda94
Andrew Stanton-Nurse <andrew@stanton-nurse.com>
2018-10-08 08:29:04 -0700

    frame for dotnet-analyze

--- src/dotnet-analyze/Program.cs
+++ src/dotnet-analyze/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 using McMaster.Extensions.CommandLineUtils;
 using Microsoft.Internal.Utilities;
 
@@ -7,6 +8,17 @@ namespace Microsoft.Diagnostics.Tools.Analyze
     [Command(Name = "dotnet-analyze", Description = "Analyzes a crash dump for known issues")]
     internal class Program
     {
+        [FileExists(ErrorMessage = "The dump file could not be found.")]
+        [Required(ErrorMessage = "You must provide a dump file to be analyzed.")]
+        [Argument(0, "<DUMP>", Description = "The path to the dump file to analyze.")]
+        public string DumpPath { get; set;}
+
+        public int OnExecute(IConsole console, CommandLineApplication app)
+        {
+            console.WriteLine("You seem to have had a bad problem and will not go to space today.");
+            return 0;
+        }
+
         private static int Main(string[] args)
         {
             DebugUtil.WaitForDebuggerIfRequested(ref args);
@@ -19,12 +31,6 @@ namespace Microsoft.Diagnostics.Tools.Analyze
             {
                 return 0;
             }
-        }
-
-        public int OnExecute(IConsole console, CommandLineApplication app)
-        {
-            console.WriteLine("You seem to have had a bad problem and will not go to space today.");
-            return 0;
         }
     }
 }
